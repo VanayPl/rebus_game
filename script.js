3e61114a14cb9998c1e8ae15428b16f9ff844215
Vviktorii <91124378+Vviktorii@users.noreply.github.com>
2022-11-08 12:40:29 +0200

    зробив дз

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+var input1 = document.getElementById('task1')
+var res = document.getElementById('result')
+var button = document.getElementById('button')
+var counter=0
+button.addEventListener('click',
+function()
+{
 
+
+if(input1.value == 'github'){
+    counter ++
+    console.log(counter)
+    button.setAttribute('disabled','true')
+    res++
+}
+    else{
+        alert('false')
+        }
+}
+
+
+)
